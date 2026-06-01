5a166dc3f4792b6b27a85991e133675609a56bae
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2026-06-01 01:56:08 -0500

    works

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('answer', { answer: 'ta da!' });
 });
 
 module.exports = router;
