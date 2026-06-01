b016daf8892cad632bb8b8386b39f0d0da2a91b7
adolfo rafael martinez <adolfo.rafael.martinez@gmail.com>
2026-06-01 02:12:36 -0500

    works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Couch' });
+  res.render('index', { title: 'Question' });
 });
 
 module.exports = router;
